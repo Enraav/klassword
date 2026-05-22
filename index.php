2b9a7231bce71ceacf750bafbcddb0a05ea2f4ea
Enraav <yhikalnik172@gmail.com>
2026-05-22 12:14:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'src/init-index.php';
+include 'src/header.php';
+?>
+
+    <main id="main" class="flex-shrink-0" role="main">
+    <h1 class="mt-5">Макияж в мурино</h1>
+    </main>
+    <img src="img/1.png" alt="фото" width="263" height="320">
+    <h6>Готовый образ макияж и прическа</h6>
+    <h4>8000.00 ₽ </h4>
+<?php include 'src/footer.php'?>
